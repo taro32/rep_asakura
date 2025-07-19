28da2cd14a3beb511c1fb6c82973264c365aa423
Yohei <yoppe777@gmail.com>
2025-07-19 14:09:49 +0900

    20250719_tchires_letkc_allQ_lamda085_psobs_target960

--- letkf_control/scale/scripts_figures/pres.py
+++ letkf_control/scale/scripts_figures/pres.py
@@ -50,7 +50,7 @@ import netCDF4 as nc
 #workdir_letkf = '/work/jh220020o/f00019/scale_enkc/test_letkc_20241029/result/case_tc/200001'
 
 workdir_letkf1 = '/work/gv42/f00019/enkc_with_TC/20250717_tchires_letkc_baseline/result/tc_hires/200001'
-workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20250718_tchires_letkc_allQ_lamda09_psobs_target960error1/result/tc_hires/200001'
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20250719_tchires_letkc_allQ_lamda09_psobs_target960error1/result/tc_hires/200001'
 
 
 day = 1
@@ -93,7 +93,7 @@ plt.plot(minpres_letkf[:],color='blue')
 plt.plot(minpres_mdet[:],color='green')
 plt.ylim(950,970)
 #plt.savefig('TCpres_nocntlvscntl_local095.png')
-plt.savefig('pres_tchires_letkc_allQ_lamda08_psobs_target960error1.png')
+plt.savefig('pres_tchires_letkf_allQ_lamda09_psobs_target960error1.png')
 plt.show()
 
 
