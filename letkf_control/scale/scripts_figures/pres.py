0e3af396072901d48f283fea53e7743bc09e5366
Yohei <yoppe777@gmail.com>
2026-02-12 10:16:10 +0900

    20260210_tchires_letkc_L1_negativeQonly_lamda095_psobs_target960error1_window1h_revise restart

--- letkf_control/scale/scripts_figures/pres.py
+++ letkf_control/scale/scripts_figures/pres.py
@@ -53,7 +53,7 @@ workdir_letkf1 = '/work/gv42/f00019/enkc_with_TC/20260209_tchires_letkc_baseline
 
 start_day = 8
 end_day = 10
-numexp = 1
+numexp = 5
 control = np.zeros((72,numexp))
 #workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20250909_tchires_letkc_L1_negativeQonly_lamda00_psobs_target960error1_window1h/result/tc_hires/200001'
 #nature, control[:,0] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
@@ -79,23 +79,33 @@ control = np.zeros((72,numexp))
 #workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20251216_tchires_letkc_L1_RI_lamda08_psobs_target990error1_window1h/result/tc_hires/200001'
 #nature, control[:,3] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
 
-workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda09_psobs_target960error1_window1h_revise/result/tc_hires/200001'
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda00_psobs_target960error1_window1h_revise/result/tc_hires/200001'
 nature, control[:,0] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda05_psobs_target960error1_window1h_revise/result/tc_hires/200001'
+nature, control[:,1] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda08_psobs_target960error1_window1h_revise/result/tc_hires/200001'
+nature, control[:,2] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
+
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda09_psobs_target960error1_window1h_revise/result/tc_hires/200001'
+nature, control[:,3] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
+
+workdir_letkf2 = '/work/gv42/f00019/enkc_with_TC/20260210_tchires_letkc_L1_negativeQonly_lamda0925_psobs_target960error1_window1h_revise/result/tc_hires/200001'
+nature, control[:,4] = calculate_min_pressure(workdir_letkf1, workdir_letkf2, start_day, end_day)
 
 
 plt.rcParams.update({'font.size': 14})  # Increase font size globally
 #print(minpres_letkf)
 #print(minpres_mdet)
 plt.plot(nature[:],color='black',label='nature',linewidth=3)
-#labels = ['$\\lambda$ = 0','$\\lambda$ = 0.5','$\\lambda$ = 0.8','$\\lambda$ = 0.9','$\\lambda$ = 0.925','$\\lambda$ = 0.95'] #,'$\\lambda$ = 0.975','$\\lambda$ = 0.99']
-labels = ['$\\lambda$ = 0.9','$\\lambda$ = 0.5','$\\lambda$ = 0.8','$\\lambda$ = 0.9','$\\lambda$ = 0.925','$\\lambda$ = 0.95'] #,'$\\lambda$ = 0.975','$\\lambda$ = 0.99']
+labels = ['$\\lambda$ = 0','$\\lambda$ = 0.5','$\\lambda$ = 0.8','$\\lambda$ = 0.9','$\\lambda$ = 0.925','$\\lambda$ = 0.95'] #,'$\\lambda$ = 0.975','$\\lambda$ = 0.99']
+#labels = ['$\\lambda$ = 0.9','$\\lambda$ = 0.5','$\\lambda$ = 0.8','$\\lambda$ = 0.9','$\\lambda$ = 0.925','$\\lambda$ = 0.95'] #,'$\\lambda$ = 0.975','$\\lambda$ = 0.99']
 #labels = ['$\\lambda$ = 0','$\\lambda$ = 0.25','$\\lambda$ = 0.5','$\\lambda$ = 0.8'] #,'$\\lambda$ = 0.975','$\\lambda$ = 0.99']
 for i in range (0,numexp):
     plt.plot(control[:,i],label=labels[i])
     #plt.plot(control[:,i])
 #plt.ylim(977,985)
 #plt.ylim(950,985)
-plt.ylim(974,982)
+plt.ylim(975,982)
 #plt.ylim(950,970)
 #plt.xlim(25,47)
 plt.xlim(0,23)
