192c5121e24b76402303879c7b7f57b40d1f65e6
Yohei <yoppe777@gmail.com>
2026-05-22 10:34:25 +0900

    reducing log level in 20260519_test

--- letkf_control/scale/run/cycle_run.sh
+++ letkf_control/scale/run/cycle_run.sh
@@ -15,7 +15,7 @@ myname="$(basename "$0")"
 job='cycle'
 
 #GROUP=${GROUP:-$(id -ng)}
-GROUP='gv42'
+GROUP='jh250035o'
 if [ "$GROUP" == "fugaku" ] ; then
   echo 'specify group name $GROUP in which you want to submit the job'
   exit 1
